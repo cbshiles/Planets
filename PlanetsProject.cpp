502f65c473aceae39cdf65350fc55f8fdca94f71
Robby Harris <john.harris297@gmail.com>
2015-04-19 12:10:16 -0400

    Fixed clipping

--- PlanetsProject.cpp
+++ PlanetsProject.cpp
@@ -16,13 +16,13 @@
 #  include <GL/glew.h>
 #  include <GL/freeglut.h>
 #  include <GL/glext.h>
-#pragma comment(lib, "glew32.lib") 
+#pragma comment(lib, "glew32.lib")
 #endif
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <iostream> 
+#include <iostream>
 #include "getbmp.h"
 
 //mouse coordinates
@@ -46,120 +46,125 @@ void loadBackgroundTexture()
 {
 		// Local storage for bmp image data.
 		BitMapFile *image[1];
-		
+
 		image[0] = getbmp("background.bmp");
-		
-		// Bind background image to texture object internalTextures[0]. 
-		glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image[0]->sizeX, image[0]->sizeY, 0, 
+
+		// Bind background image to texture object internalTextures[0].
+		glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image[0]->sizeX, image[0]->sizeY, 0,
 	            GL_RGBA, GL_UNSIGNED_BYTE, image[0]->data);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-		
+
 }
 
 void drawScene(void)
 {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    
+
     gluLookAt(
-    my_x, 
-    my_y, 
-    my_z, 
-    
-    my_x - 5*sin((M_PI/180.0) * my_angle),  
-    my_y - 5*cos((M_PI/180.0) * my_angle_y), 
-    my_z - 5*cos((M_PI/180.0) * my_angle), 
-    
-    0, 
-    1, 
+    my_x,
+    my_y,
+    my_z,
+
+    my_x - 5*sin((M_PI/180.0) * my_angle),
+    my_y - 5*cos((M_PI/180.0) * my_angle_y),
+    my_z - 5*cos((M_PI/180.0) * my_angle),
+
+    0,
+    1,
     0);
-    
-    glColor3f(1, 1, 1);
+
+    //glColor3f(1, 1, 1);
     //create a box around the current viewpoint
     	//front
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(-200.0, -200.0, 200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(200.0, -200.0, 200.0);
-		glTexCoord2f(1, 1); glVertex3f(200.0, 200.0, 200.0);
-		glTexCoord2f(0.0, 1); glVertex3f(-200.0, 200.0, 200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-100.0, -100.0, 100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(100.0, -100.0, 100.0);
+		glTexCoord2f(1, 1); glVertex3f(100.0, 100.0, 100.0);
+		glTexCoord2f(0.0, 1); glVertex3f(-100.0, 100.0, 100.0);
 	glEnd();
-   
+
+    //glColor3f(0, 1, 0);
 	//bottom
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(-200.0, -200.0, 200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(200.0, -200.0, 200.0);
-		glTexCoord2f(1, 1); glVertex3f(200.0, -200.0, -200.0);
-		glTexCoord2f(0.0, 1); glVertex3f(-200.0, -200.0, -200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-100.0, -100.0, 100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(100.0, -100.0, 100.0);
+		glTexCoord2f(1, 1); glVertex3f(100.0, -100.0, -100.0);
+		glTexCoord2f(0.0, 1); glVertex3f(-100.0, -100.0, -100.0);
 	glEnd();
-   
+
+    //glColor3f(0, 0, 1);
 	//top
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(-200.0, 200.0, 200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(200.0, 200.0, 200.0);
-		glTexCoord2f(1, 1); glVertex3f(200.0, 200.0, -200.0);
-		glTexCoord2f(0.0, 1); glVertex3f(-200.0, 200.0, -200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-100.0, 100.0, 100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(100.0, 100.0, 100.0);
+		glTexCoord2f(1, 1); glVertex3f(100.0, 100.0, -100.0);
+		glTexCoord2f(0.0, 1); glVertex3f(-100.0, 100.0, -100.0);
 	glEnd();
-   
+
+    //glColor3f(1, 1, 0); //purple
 	//left
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(-200.0, -200.0, 200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(-200.0, 200.0, 200.0);
-		glTexCoord2f(1, 1); glVertex3f(-200.0, 200.0, -200.0);;
-		glTexCoord2f(0.0, 1); glVertex3f(-200.0, -200.0, -200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-100.0, -100.0, 100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(-100.0, 100.0, 100.0);
+		glTexCoord2f(1, 1); glVertex3f(-100.0, 100.0, -100.0);;
+		glTexCoord2f(0.0, 1); glVertex3f(-100.0, -100.0, -100.0);
 	glEnd();
-   
+
+    //glColor3f(1, 0, 1); //yellow
 	//right
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(200.0, 200.0, 200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(200.0, -200.0, 200.0);
-		glTexCoord2f(1, 1); glVertex3f(200.0, -200.0, -200.0);;
-		glTexCoord2f(0.0, 1); glVertex3f(200.0, 200.0, -200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(100.0, 100.0, 100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(100.0, -100.0, 100.0);
+		glTexCoord2f(1, 1); glVertex3f(100.0, -100.0, -100.0);;
+		glTexCoord2f(0.0, 1); glVertex3f(100.0, 100.0, -100.0);
 	glEnd();
-   
+
+    //glColor3f(0, 1, 1); //cyan
 	//back
-	glBindTexture(GL_TEXTURE_2D, externalTextures[0]); 
+	glBindTexture(GL_TEXTURE_2D, externalTextures[0]);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 0.0); glVertex3f(-200.0, -200.0, -200.0);
-		glTexCoord2f(1, 0.0); glVertex3f(200.0, -200.0, -200.0);
-		glTexCoord2f(1, 1); glVertex3f(200.0, 200.0, -200.0);
-		glTexCoord2f(0.0, 1); glVertex3f(-200.0, 200.0, -200.0);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-100.0, -100.0, -100.0);
+		glTexCoord2f(1, 0.0); glVertex3f(100.0, -100.0, -100.0);
+		glTexCoord2f(1, 1); glVertex3f(100.0, 100.0, -100.0);
+		glTexCoord2f(0.0, 1); glVertex3f(-100.0, 100.0, -100.0);
 	glEnd();
-    
-		
-    glColor3f(0, 0, 0);
+
+
+    glColor3f(1, 1, 1);
     glTranslatef(0, 0, -20);
     glutSolidSphere(5, 50, 50);
-    
+
     glutSwapBuffers();
 }
 
 // Initialization routine.
-void setup(void) 
+void setup(void)
 {
-		glClearColor(1.0, 1.0, 1.0, 0.0); 
+		glClearColor(1.0, 1.0, 1.0, 0.0);
 		glEnable(GL_DEPTH_TEST);
     glEnable(GL_POLYGON_OFFSET_FILL);
     glPolygonOffset( 1, 1);
-    
+
     //TEXTURES
-    glGenTextures(1, externalTextures);
-    
+    //glGenTextures(1, externalTextures);
+
 		glPushMatrix();
 		glTranslatef(0, 0, -100);
 		loadBackgroundTexture();
 		glPopMatrix();
-		
+
 		// Specify how texture values combine with current surface color values.
-		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE); 
+		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
 		// Turn on OpenGL texturing.
 		glEnable(GL_TEXTURE_2D);
@@ -183,45 +188,59 @@ void resize(int w, int h)
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-   switch(key) 
+   switch(key)
    {
 		case 27:                  // escape
 			exit(0);
 			break;
-		case 'd': 
-			my_x+=sin(M_PI/180*(my_angle + 90));
-			my_z+=cos(M_PI/180*(my_angle + 90));
+		case 'd':
+			if(my_x < 100){
+                my_x+=sin(M_PI/180*(my_angle + 90));
+			}
+			if(my_z < 100 && my_z > 0.1){
+                my_z+=cos(M_PI/180*(my_angle + 90));
+			}
 			break;
-		case 'a': 
-			my_x-=sin(M_PI/180*(my_angle + 90));
-			my_z-=cos(M_PI/180*(my_angle + 90));
+		case 'a':
+			if(my_x > -100){
+                my_x-=sin(M_PI/180*(my_angle + 90));
+			}
+			if(my_z < -100 && my_z > 0.1){
+                my_z-=cos(M_PI/180*(my_angle + 90));
+			}
 			break;
 		case 'w':
-			my_x -= sin((M_PI/180.0)*my_angle);
-			my_z -= cos((M_PI/180.0)*my_angle);
+            if(my_x < 100 && my_x > -100){
+                my_x -= sin((M_PI/180.0)*my_angle);
+            }
+			if(my_z > -100){
+                my_z -= cos((M_PI/180.0)*my_angle);
+			}
 			break;
 		case 's':
-			my_x += sin((M_PI/180.0)*my_angle);
-			my_z += cos((M_PI/180.0)*my_angle);
+            if(my_x < 100 && my_x > -100){
+                my_x -= sin((M_PI/180.0)*my_angle);
+            }
+			if(my_z < 0.1)
 			break;
    }
-   
+
     if (my_angle > 360.0) my_angle -= 360;
     if (my_angle < 0) my_angle += 360;
-    
+
     if (my_x>180)
     {
 				my_x=180;
 		}
-		
+
 		if (my_y>180) {
 				my_y=180;
 		}
-		
+
 		if (my_z>180) {
 				my_z=180;
     }
-    
+
     glutPostRedisplay();
 }
 
@@ -234,14 +253,14 @@ void trackMouse (int x, int y)
 
 //moves the camera depending on the location of the mouse
 void mouseMotion()
-{	
+{
 	//left right
 	if (last_mouse_x<20) my_angle+=0.1;
 	if (last_mouse_x>480) my_angle-=0.1;
-	
+
 	if (my_angle > 360.0) my_angle -= 360;
     if (my_angle < 0) my_angle += 360;
-	
+
 	//up down
 	if (last_mouse_y>20) my_angle_y+=0.1;
 	if (last_mouse_y<480) my_angle_y-=0.1;
@@ -253,15 +272,15 @@ void mouseMotion()
 }
 
 // Main routine.
-int main(int argc, char **argv) 
-{ 
+int main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA); 
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
 	glutInitWindowSize(600, 600);
-	glutInitWindowPosition(100, 100); 
-	glutCreateWindow("planets");	
-	glutDisplayFunc(drawScene); 
-	glutReshapeFunc(resize);  
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("planets");
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyInput);
 	glutPassiveMotionFunc(trackMouse);
 	glutIdleFunc(mouseMotion);
@@ -269,7 +288,7 @@ int main(int argc, char **argv)
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	setup(); 
-   
-	glutMainLoop(); 
+	setup();
+
+	glutMainLoop();
 }
